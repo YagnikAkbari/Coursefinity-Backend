f98cb6572f6115b86c65eb089f7baa321abc5541
YagnikAkbari2 <yagnik_20003@ldrp.ac.in>
2023-08-12 13:40:10 +0530

    remove unnecessary code from webhook

--- server.js
+++ server.js
@@ -52,22 +52,6 @@ app.post(
         // Then define and call a method to handle the successful payment intent.
         // handlePaymentIntentSucceeded(paymentIntent);
         break;
-      case "payment_method.attached":
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        break;
-      case "subscription_schedule.canceled":
-        const subscriptionScheduleCanceled = event.data.object;
-        // Then define and call a function to handle the event subscription_schedule.canceled
-        break;
-      case "charge.captured":
-        const chargeCaptured = event.data.object;
-        // Then define and call a function to handle the event charge.captured
-        break;
-      case "invoice.payment_succeeded":
-        const invoicePaymentSucceeded = event.data.object;
-        // Then define and call a function to handle the event invoice.payment_succeeded
-        break;
       default:
         console.log(`Unhandled event type ${event.type}.`);
     }
