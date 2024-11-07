4761476025be9aff4a640ec68869d9958b3276ef
Yagnik Akbari <yagnik58ppsv@gmail.com>
2024-11-07 07:11:27 +0530

    middleware update from common to specific route for instructor.

--- routes/instructor.js
+++ routes/instructor.js
@@ -5,13 +5,35 @@ const courseController = require("../controller/course");
 const authController = require("../controller/auth");
 const multer = require("multer");
 const { uploadImage } = require("../controller/upload/image");
+const common = require("../middlewares/index");
 
 const upload = multer({ dest: "images/", fieldname: "image" });
-router.use(checkInstructor);
-router.get("/mycreatedcourses", courseController.getUserCreatedCourses);
-router.post("/createCourse", courseController.createCourse);
-router.post("/uploadThumbnail", upload.single("image"), uploadImage);
-router.delete("/deletecourse", courseController.deleteCourse);
-router.get("/instructorDetails", authController.getInstructorDetails);
+
+router.get(
+  "/mycreatedcourses",
+  common.roleBasedMiddleware("instructor"),
+  courseController.getUserCreatedCourses
+);
+router.post(
+  "/createCourse",
+  common.roleBasedMiddleware("instructor"),
+  courseController.createCourse
+);
+router.post(
+  "/uploadThumbnail",
+  common.roleBasedMiddleware("instructor"),
+  upload.single("image"),
+  uploadImage
+);
+router.delete(
+  "/deletecourse",
+  common.roleBasedMiddleware("instructor"),
+  courseController.deleteCourse
+);
+router.get(
+  "/instructorDetails",
+  common.roleBasedMiddleware("instructor"),
+  authController.getInstructorDetails
+);
 
 module.exports = router;
