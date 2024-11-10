da1dee4d7e244104b70a3582d7c682165e059652
Yagnik Akbari <yagnik58ppsv@gmail.com>
2024-11-10 16:16:06 +0530

    resolved the serverless function issue.

--- routes/instructor.js
+++ routes/instructor.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const checkInstructor = require("../middlewares/instructorMiddlewares/auth");
 const courseController = require("../controller/course");
 const authController = require("../controller/auth");
 const multer = require("multer");
+const path = require("path");
 const { uploadImage } = require("../controller/upload/image");
 const common = require("../middlewares/index");
 
-const upload = multer({ dest: "images/", fieldname: "image" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "tmp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get(
   "/mycreatedcourses",
