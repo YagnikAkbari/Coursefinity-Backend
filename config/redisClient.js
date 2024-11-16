1504946d0cac1a3ad1c50803475b9cd4260bd7e6
Yagnik Akbari <yagnik58ppsv@gmail.com>
2024-11-16 16:59:17 +0530

    hosted version logic added for caching on redis

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,12 @@
 const redis = require("redis");
 
-const redisClient = redis.createClient().on("error", (error) => {
-  console.log("ERROR:- REDIS", error);
-  throw error;
-});
+const redisClient = redis
+  .createClient({
+    url: process.env.REDIS_URL,
+  })
+  .on("error", (error) => {
+    console.log("ERROR:- REDIS", error);
+    throw error;
+  });
 
 module.exports = { redisClient };
