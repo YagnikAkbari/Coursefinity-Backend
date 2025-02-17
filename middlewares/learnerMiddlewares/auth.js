37e476e5abebd39b18b6591448c2f6b2352c2066
Yagnik Akbari <yagnik58ppsv@gmail.com>
2025-02-17 20:02:00 +0530

    change logic for json token expired.

--- middlewares/learnerMiddlewares/auth.js
+++ middlewares/learnerMiddlewares/auth.js
@@ -3,24 +3,32 @@ const learner = require("../../model/learner");
 
 const checkLearner = async (req, res, next) => {
   try {
-    
     const token = req.headers?.authorization?.split(" ")[1] ?? null;
     if (!token) {
       return res.status(401).send({ message: "Unauthenticated." });
     }
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    
-    if (!user?.id) {
-      return res.status(401).send({ message: "Unauthenticated." });
-    }
-    const student = await learner.findOne({ email: user?.email });
-    if (!student) {
-      return res.status(401).send({ message: "Unauthenticated." });
-    }
-    
-    req.learner = student;
-    next();
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
+      if (err) {
+        if (err?.name === "TokenExpiredError") {
+          return res.status(401).json({ message: "JWT expired" });
+        }
+        return res
+          .status(403)
+          .json({ message: "Failed to authenticate token" });
+      }
+      if (!decode?.id) {
+        return res.status(401).send({ message: "Unauthenticated." });
+      }
+      const student = await learner.findOne({ email: decode?.email });
+      if (!student) {
+        return res.status(401).send({ message: "Unauthenticated." });
+      }
+      req.learner = student;
+      next();
+    });
   } catch (error) {
+    console.log("errorerror", error);
+
     next(error);
   }
 };
